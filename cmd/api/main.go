28600c8cf9f56784bab25fce485b2211658d4154
Mahdi Hasani <m2hdtl@gmail.com>
2022-03-28 09:05:24 +0430

    describe kernel sections

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,29 +10,37 @@ import (
 	"github.com/smhdhsn/bookstore-user/internal/service"
 )
 
+// main is the application's kernel.
 func main() {
+	// read configurations
 	conf, err := config.LoadConf()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// create a database connection
 	dbConn, err := db.Connect(conf.DB)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// initialize auto migration
 	if err := db.InitMigrations(dbConn); err != nil {
 		log.Fatal(err)
 	}
 
+	// instanciate repositories
 	uRepo := mysql.NewUserRepo(dbConn)
 
+	// instanciate services
 	uServ := service.NewUserService(uRepo)
 
+	// instanciate handlers
 	httpServer := http.New(uServ)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// listen and serve
 	log.Fatal(httpServer.Listen(conf.Server))
 }
